0f8b8451d16febc8700f6e1426723d203cf9e01a
Yaroslav Furman <yaro330@gmail.com>
2024-02-11 17:41:57 +0800

    cpufreq_schedutil: remove tracing

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -211,8 +211,6 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
     else
             freq = (freq + (freq >> 2)) * util / max;
 
-	trace_sugov_next_freq(policy->cpu, util, max, freq);
-
 	if (freq == sg_policy->cached_raw_freq && sg_policy->next_freq != UINT_MAX)
 		return sg_policy->next_freq;
 	sg_policy->cached_raw_freq = freq;
